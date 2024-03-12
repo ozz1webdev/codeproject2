a7bd3fde1e4318dfd323d3cf95b05cb0180d2fa2
ozz1webdev <ozz1webdev@gmail.com>
2024-03-12 11:56:58 +0100

    add rotate screen function

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,3 +1,11 @@
+function screenRotate () {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        $('body').css({
+            "-webkit-transform": "rotate(90deg)"
+        }); 
+    }
+    
+}
 function startGame() {
     document.getElementById("startPage").style.display = "none";
     document.getElementById("gameScreen").style.display = "flex";
