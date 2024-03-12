78879e2d1621fdaf15dbda59c52a132e7fe4463b
ozz1webdev <ozz1webdev@gmail.com>
2024-03-12 11:57:31 +0100

    add toggle fullscreen function

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,12 +1,23 @@
-function screenRotate () {
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        $('body').css({
-            "-webkit-transform": "rotate(90deg)"
-        }); 
+var btnFullScreen = document.getElementById("btnFullScreen");
+btnFullScreen.addEventListener("click", toggleFullScreen);
+
+function toggleFullScreen() {
+    var elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
     }
-    
+}
+function onStart () {
+
 }
 function startGame() {
     document.getElementById("startPage").style.display = "none";
     document.getElementById("gameScreen").style.display = "flex";
 }
+
