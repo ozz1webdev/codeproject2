8e79da341c8240f15404949ff81e0d0736174e50
ozz1webdev <ozz1webdev@gmail.com>
2024-03-15 16:43:27 +0100

    add check win

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -11,13 +11,18 @@ function roll(reel,offset) {
         targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
         normTargetBackgroundPositionY = targetBackgroundPositionY % (num_icons * icon_height);
     
-    reel.style.transition = `background-position-y ${8 + delta * time_per_icon}ms`;
+    reel.style.transition = `background-position-y ${8 + delta * time_per_icon}ms cubic-bezier(.45,.05,.58,1.09)`;
     reel.style["background-position-y"] = `${targetBackgroundPositionY}px`;
-    reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
+    //reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
     
     return delta%num_icons;
 }
-
+function checkWin() {
+    const reelsList = document.querySelectorAll('.reel-container > .reel');
+    if (reelsList[0].style["background-position-y"] === "0px" && reelsList[1].style["background-position-y"] === "79px" && reelsList[2].style["background-position-y"] === "158px") {
+        alert("You won!");
+    }
+}
 function rollAll() {
     const reelsList = document.querySelectorAll('.reel-container > .reel');
     
@@ -25,6 +30,6 @@ function rollAll() {
         let delta = roll(reelsList[i],indexes[i]);
         indexes[i] = (indexes[i] + delta) % num_icons;
     }
-    console.log(indexes);
+    checkWin();
 }
 rollAll();
