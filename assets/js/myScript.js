01435d49eb5de73197f645daa0b78b4122fa5e04
ozz1webdev <ozz1webdev@gmail.com>
2024-03-19 13:17:46 +0100

    changin the reel code and adding sections

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,47 +1,79 @@
-const icon_width = 79,
-    icon_height = 79,
-    num_icons = 9,
-    time_per_icon = 100;
-let indexes = [0,0,0];
-
-function roll(reel,offset) {
-    const delta = (offset + 2) * num_icons + Math.round(Math.random() * num_icons);
-    const style = getComputedStyle(reel),
-        backgroundPositionY = parseFloat(style["background-position-y"]),
-        targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
-        normTargetBackgroundPositionY = targetBackgroundPositionY % (num_icons * icon_height);
-    
-    reel.style.transition = `background-position-y ${8 + delta * time_per_icon}ms cubic-bezier(.45,.05,.58,1.09)`;
-    reel.style["background-position-y"] = `${targetBackgroundPositionY}px`;
-    //reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
+// Width of the icons
+icon_width = 79,	
+// Height of one icon in the strip
+icon_height = 79,	
+// Number of icons in the strip
+num_icons = 9,	
+// Max-speed in ms for animating one icon down
+time_per_icon = 100,
+// Holds icon indexes
+indexes = [0, 0, 0];
 
 
-        return delta%num_icons;
-        
-    
-}
-function checkWin() {
-    if (indexes[0] == indexes[1] || indexes[1] == indexes[2] || indexes[0] == indexes[2]) {
-        alert("win 2 same icons");
-    }
-    if (indexes[0] == indexes[1] ==  indexes[2]) {
-        alert("win 3 same icons");
-    }
-}
+/** 
+ * Roll one reel
+ */
+const roll = (reel, offset = 0) => {
+	// Minimum of 2 + the reel offset rounds
+	const delta = (offset + 2) * num_icons + Math.round(Math.random() * num_icons); 
+	
+	// Return promise so we can wait for all reels to finish
+	return new Promise((resolve, reject) => {
+		
+		const style = getComputedStyle(reel),
+					// Current background position
+					backgroundPositionY = parseFloat(style["background-position-y"]),
+					// Target background position
+					targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
+					// Normalized background position, for reset
+					normTargetBackgroundPositionY = targetBackgroundPositionY%(num_icons * icon_height);
+		
+		// Delay animation with timeout, for some reason a delay in the animation property causes stutter
+		setTimeout(() => { 
+			// Set transition properties ==> https://cubic-bezier.com/#.41,-0.01,.63,1.09
+			reel.style.transition = `background-position-y ${(8 + 1 * delta) * time_per_icon}ms cubic-bezier(.41,-0.01,.63,1.09)`;
+			// Set background position
+			reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
+		}, offset * 150);
+			
+		// After animation
+		setTimeout(() => {
+			// Reset position, so that it doesn't get higher without limit
+			reel.style.transition = `none`;
+			reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
+			// Resolve this promise
+			resolve(delta%num_icons);
+		}, (8 + 1 * delta) * time_per_icon + offset * 150);
+		
+	});
+};
 
 
+/**
+ * Roll all reels, when promise resolves roll again
+ */
 function rollAll() {
-    const reelsList = document.querySelectorAll('.reel-container > .reel');
-    
-    let delta;
-    
-    for (let i=0;i<3;i++) {
-        delta = roll(reelsList[i],indexes[i]);
-        indexes[i] = (indexes[i] + delta) % num_icons;
-        
-    }
-    console.log('delta :',delta);
-    console.log('indexes:',indexes);
-    //checkWin();
-}
-rollAll();
+	
+	const reelsList = document.querySelectorAll('.reel-container > .reel');
+	
+	Promise
+		
+		// Activate each reel, must convert NodeList to Array for this with spread operator
+		.all( [...reelsList].map((reel, i) => roll(reel, i)) )	
+		
+		// When all reels done animating (all promises solve)
+		.then((deltas) => {
+			// add up indexes
+			deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta)%num_icons);
+		
+			// Win conditions
+			if (indexes[0] == indexes[1] || indexes[1] == indexes[2]) {
+				const winCls = indexes[0] == indexes[2] ? "win2" : "win1";
+				alert("win");
+			}
+		
+			
+		});
+};
+
+// 
